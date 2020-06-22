f32c4e8ef0c2c64974e413c1961766a766bbe97f
Vignesh  Sivarajah <vigneshsivarajah@Vigneshs-MacBook-Pro.local>
2020-06-22 22:30:35 +0200

    updated application deploy script and added a k8s spec file

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=ml_microservice .
+docker build --tag=vigneshsivarajah/ml_microservice .
 # Step 2: 
 # List docker images
 docker image ls 
