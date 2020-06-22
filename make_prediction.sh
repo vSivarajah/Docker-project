718a4a1b0553483f179b69203e5242f3830d11ff
Vignesh  Sivarajah <vigneshsivarajah@Vigneshs-MacBook-Pro.local>
2020-06-22 22:41:17 +0200

    added log outputs for docker and kubernetes.

--- make_prediction.sh
+++ make_prediction.sh
@@ -25,4 +25,4 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST http://localhost:$PORT/predict
+     -X POST http://127.0.0.1:$PORT/predict
--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -16,4 +16,4 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward deployment/houseprediction 8080:80
+kubectl port-forward deployment/houseprediction 8000:80
